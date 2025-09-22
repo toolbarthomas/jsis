5e448bd336f6d12e8370261f9cf5411c5b6d71e8
Thomas van der Velde <contact@toolbarthomas.net>
2025-09-22 22:48:19 +0200

    feat(core): Implement read/write

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,40 @@
-export type FieldArguments = {
-  type?: (typeof JSIS.types)[number]
-  size?: number
-  key?: string
-}
-
-export type Field = FieldArguments & {
-  name?: string
-  blocks: number
-  index: number
-}
-
-export type Schema = {
-  range: number
-  fields: Record<string, Field>
-}
-
-export type Encodable = string | boolean | number
-export type Decodable = number | number[]
-
-export type ROM = Int16Array
+import { Encodable, Decodable, Field, FieldArguments, ROM, Schema } from './types'
 
 class JSIS {
+  /**
+   * Defines the available Types to store within the Integer Array
+   */
   static types = ['boolean', 'float', 'integer', 'string'] as const
 
+  /**
+   * Fixed ranges for the available types which are bound to the Integer Array
+   * range.
+   */
   static INTEGER = 4
   static FLOAT = 8
   static STRING = 2
   static BOOLEAN = 1
   static ENDIAN = true
-
-  static ROWS = 1
   static BITS = 16
   static RANGE = 0xffff
+  static BLANK = 0
 
+  /**
+   * The minimum row amount for a Schematic to use
+   */
+  static ROWS = 1
+
+  /**
+   * Internal Integer Storage Buffer and Array View that is required while
+   * parsing Integer values.
+   */
   static integerBuffer = new ArrayBuffer(JSIS.INTEGER)
   static integerView = new DataView(JSIS.integerBuffer, 0)
 
+  /**
+   * Internal Floating Point Storage Buffer and Array View that is required
+   * while parsing Floating Points.
+   */
   static floatBuffer = new ArrayBuffer(JSIS.FLOAT)
   static floatView = new DataView(JSIS.floatBuffer, 0)
 
@@ -98,7 +97,12 @@ class JSIS {
             ? JSIS.INTEGER / 2
             : type === 'boolean'
               ? 1
-              : Math.ceil(Math.max((field.size || 0) * JSIS.STRING, JSIS.STRING))
+              : Math.ceil(
+                  Math.max(
+                    (field.size || (type === 'string' ? JSIS.BITS : 0)) * JSIS.STRING,
+                    JSIS.STRING
+                  )
+                )
 
       schema.fields[key] = {
         name: field.key,
@@ -111,6 +115,142 @@ class JSIS {
     }
 
     return schema as T
+  }
+
+  /**
+   * Decodes value from the existing Integer Array context and parse it in the
+   * expected type: Boolean, String, Float or Integer.
+   *
+   * @param value The value to decode
+   * @param type Parse as the defined type instead.
+   */
+  static decode(value?: Decodable, type?: FieldArguments['type']) {
+    if (value === undefined || typeof value === 'number') {
+      if (!type || type === 'boolean') {
+        return value ? true : false
+      }
+
+      return value
+    }
+
+    if (value.length === JSIS.BOOLEAN && (value[0] === 1 || !value[0])) {
+      return value[0] ? true : false
+    }
+
+    let index = 0
+    const isNumber = type === 'float' || type === 'integer'
+
+    // Convert the existing Float or Integer with the internal Array Buffers
+    // which should be correctly encoded for the given Integer Array.
+    while (isNumber && index < value.length) {
+      const currentIndex = index
+
+      index++
+
+      if (type === 'float') {
+        JSIS.floatView.setInt16(currentIndex * 2, value[currentIndex], JSIS.ENDIAN)
+
+        continue
+      }
+
+      if (type === 'integer' || (type === undefined && value.length === JSIS.INTEGER / 2)) {
+        JSIS.integerView.setInt16(currentIndex * 2, value[currentIndex], JSIS.ENDIAN)
+
+        continue
+      }
+    }
+
+    // Parse the assumed bits to the actual value
+    switch (type) {
+      case 'float':
+        return JSIS.floatView.getFloat64(0, JSIS.ENDIAN)
+
+      case 'string':
+        const characters: string[] = new Array(value.length / JSIS.STRING)
+
+        while (index < characters.length) {
+          characters[index] = String.fromCharCode(
+            (value[index * JSIS.STRING + 1] << JSIS.BITS) | (value[index * JSIS.STRING] & 0xffff)
+          )
+
+          index++
+        }
+
+        return characters.join('')
+
+      default:
+        return value.length === JSIS.INTEGER / 2
+          ? JSIS.integerView.getInt32(0, JSIS.ENDIAN)
+          : undefined
+    }
+  }
+
+  /**
+   * Encodes the defined value for the existing Integer Array
+   * @param value
+   */
+  static encode(value?: Encodable) {
+    if (value === undefined) {
+      return
+    }
+
+    const type = typeof value
+
+    if (type !== 'number' && !JSIS.types.includes(type)) {
+      return
+    }
+
+    let response: undefined | Int16Array = undefined
+    let index = 0
+
+    switch (type) {
+      case 'boolean':
+        return value ? 1 : 0
+
+      case 'string':
+        const encoded = new Int16Array(value.length * JSIS.STRING)
+
+        while (index < value.length) {
+          const code = value.charCodeAt(index)
+          const pointer = index * JSIS.STRING
+
+          encoded[pointer] = code & JSIS.RANGE
+          encoded[pointer + 1] = code >> JSIS.BITS // high bits (usually 0 for BMP)
+
+          index++
+        }
+
+        return encoded
+
+      default:
+        // // Does not work
+        // if (value < JSIS.MIN || value > JSIS.MAX) {
+        //   throw Error(`Integer overflow detected: ${Math.abs(value)}/${JSIS.MAX}`)
+        // }
+
+        const blocks = Number.isInteger(value)
+          ? Math.floor(JSIS.INTEGER / 2)
+          : Math.floor(JSIS.FLOAT / 2)
+
+        response = new Array(blocks)
+        const isInteger = Number.isInteger(value)
+
+        if (isInteger) {
+          JSIS.integerView.setInt32(0, value, JSIS.ENDIAN)
+        } else {
+          JSIS.floatView.setFloat64(0, value, JSIS.ENDIAN)
+        }
+
+        while (index < blocks) {
+          response[index] = isInteger
+            ? JSIS.integerView.getInt16(index * 2, JSIS.ENDIAN)
+            : JSIS.floatView.getInt16(index * 2, JSIS.ENDIAN)
+
+          index++
+        }
+
+        return response
+    }
   }
 
   /**
@@ -159,153 +299,50 @@ class JSIS {
       return
     }
 
+    const k = key.toLowerCase()
     const pointer = JSIS.getPointer(key, schema, row)
-    const blocks = schema.fields[key.toLowerCase()].blocks
+    const blocks = schema.fields[k].blocks
     const chunk = rom.subarray(pointer, pointer + blocks)
+    const type = schema.fields[k].type
 
-    return chunk
+    console.log('AA', key, chunk, type)
+
+    return JSIS.decode(chunk, type)
   }
 
-  /**
-   * Decodes value from the existing Integer Array context and parse it in the
-   * expected type: Boolean, String, Float or Integer.
-   *
-   * @param value The value to decode
-   * @param type Parse as the defined type instead.
-   */
-  static decode(value?: Decodable, type?: FieldArguments['type']) {
-    if (value === undefined || typeof value === 'number') {
-      if (!type || type === 'boolean') {
-        return value ? true : false
-      }
-
-      return value
-    }
-
-    let index = 0
-    const isNumber = type === 'float' || type === 'integer'
-
-    // Convert the existing Float or Integer with the internal Array Buffers
-    // which should be correctly encoded for the given Integer Array.
-    while (isNumber && index < value.length) {
-      const currentIndex = index
-
-      index++
-
-      if (type === 'float') {
-        JSIS.floatView.setInt16(currentIndex * 2, value[currentIndex], JSIS.ENDIAN)
-
-        continue
-      }
-
-      if (type === 'integer' || (type === undefined && value.length === JSIS.INTEGER / 2)) {
-        JSIS.integerView.setInt16(currentIndex * 2, value[currentIndex], JSIS.ENDIAN)
-
-        continue
-      }
-    }
-
-    // Parse the assumed bits to the actual value
-    switch (type) {
-      case 'float':
-        return JSIS.floatView.getFloat64(0, JSIS.ENDIAN)
-
-      case 'string':
-        // const string: string[] = Array(value.length / JSIS.STRING)
-
-        // while (index < string.length) {
-        //   const code = (value[index * 2 + 1] << (JSIS.BITS - 1)) | (value[index * 2] & JSIS.RANGE)
-        //   string[index] = String.fromCharCode(code)
-
-        //   index++
-        // }
-
-        // return string.join('')
-
-        const characters: string[] = new Array(value.length / JSIS.STRING)
-
-        while (index < characters.length) {
-          characters[index] = String.fromCharCode(
-            (value[index * JSIS.STRING + 1] << JSIS.BITS) | (value[index * JSIS.STRING] & 0xffff)
-          )
-
-          index++
-        }
-
-        return characters.join('')
-
-      default:
-        return value.length === JSIS.INTEGER / 2
-          ? JSIS.integerView.getInt32(0, JSIS.ENDIAN)
-          : undefined
-    }
-  }
-
-  /**
-   * Encodes the defined value for the existing Integer Array
-   * @param value
-   * @returns
-   */
-  static encode(value?: Encodable) {
-    if (value === undefined) {
+  static write(key: string, value: Encodable, schema: Schema, rom: ROM, row?: number) {
+    if (!key || value === undefined || !schema || !rom) {
       return
     }
 
-    const type = typeof value
+    const k = key.toLowerCase()
 
-    if (type !== 'number' && !JSIS.types.includes(type)) {
+    if (!schema.fields[k]) {
       return
     }
 
-    let response: undefined | Int16Array = undefined
-    let index = 0
+    const encoded = JSIS.encode(value)
+    const pointer = JSIS.getPointer(k, schema, row)
 
-    switch (type) {
-      case 'boolean':
-        return value ? 1 : 0
-
-      case 'string':
-        const encoded = new Int16Array(value.length * JSIS.STRING)
-
-        while (index < value.length) {
-          const code = value.charCodeAt(index)
-          encoded[index * JSIS.STRING] = code & JSIS.RANGE
-          encoded[index * JSIS.STRING + 1] = code >> JSIS.BITS // high bits (usually 0 for BMP)
-
-          index++
-        }
-
-        return encoded
-
-      default:
-        // // Does not work
-        // if (value < JSIS.MIN || value > JSIS.MAX) {
-        //   throw Error(`Integer overflow detected: ${Math.abs(value)}/${JSIS.MAX}`)
-        // }
-
-        const blocks = Number.isInteger(value)
-          ? Math.floor(JSIS.INTEGER / 2)
-          : Math.floor(JSIS.FLOAT / 2)
-
-        response = new Array(blocks)
-        const isInteger = Number.isInteger(value)
-
-        if (isInteger) {
-          JSIS.integerView.setInt32(0, value, JSIS.ENDIAN)
-        } else {
-          JSIS.floatView.setFloat64(0, value, JSIS.ENDIAN)
-        }
-
-        while (index < blocks) {
-          response[index] = isInteger
-            ? JSIS.integerView.getInt16(index * 2, JSIS.ENDIAN)
-            : JSIS.floatView.getInt16(index * 2, JSIS.ENDIAN)
-
-          index++
-        }
-
-        return response
+    if (encoded === undefined) {
+      return
     }
+
+    if (typeof encoded === 'number') {
+      rom[pointer] = encoded
+
+      return true
+    }
+
+    let blocks = schema.fields[k].blocks
+
+    while (blocks) {
+      blocks--
+
+      rom[pointer + blocks] = encoded.length < blocks ? JSIS.BLANK : encoded[blocks]
+    }
+
+    return true
   }
 }
 
@@ -316,9 +353,13 @@ const { rom, schema } = JSIS.create(
     type: 'string'
   },
   {
+    key: 'Budget',
+    type: 'float'
+  },
+  {
     key: 'Lastname',
     type: 'string',
-    size: 32
+    size: 13
   },
   {
     key: 'Age',
@@ -334,17 +375,30 @@ const { rom, schema } = JSIS.create(
 )
 
 console.log('Schema', schema, rom)
-// console.log('Read', JSIS.read('lastname', schema, rom, 1))
+console.log(
+  'Write',
+  JSIS.write('firstname', 'John Doe', schema, rom),
+  JSIS.write('budget', Math.PI, schema, rom),
+  JSIS.write('age', 0xfffff, schema, rom),
+  JSIS.write('firstname', 'Jane Doe', schema, rom, 1),
+  JSIS.write('subscribed', true, schema, rom),
+  JSIS.write('subscribed', false, schema, rom, 1)
+)
+console.log(
+  'Read',
+  JSIS.read('firstname', schema, rom),
+  JSIS.read('budget', schema, rom),
+  JSIS.read('age', schema, rom),
+  JSIS.read('subscribed', schema, rom),
+  JSIS.read('firstname', schema, rom, 1),
+  JSIS.read('subscribed', schema, rom, 1)
+)
+
 // console.log('True', JSIS.encode(true))
 // console.log('False', JSIS.encode(false))
 // console.log('Float', JSIS.encode(Math.PI), JSIS.decode(JSIS.encode(Math.PI), 'float'), Math.PI)
 // console.log('Integer', JSIS.encode(0x1000000), JSIS.decode(JSIS.encode(0x1000000)), 0x1000000)
-console.log(
-  'String',
-  JSIS.encode('Lorem🤫'),
-  JSIS.decode(JSIS.encode('Lorem🤫'), 'string'),
-  'Lorem🤫'
-)
+console.log('String', JSIS.decode(JSIS.encode('Lorem🤫'), 'string'), 'Lorem🤫')
 
 // console.log('Boolean', JSIS.MAX, JSIS.encode(true), JSIS.decode(JSIS.encode(true)), true)
 
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,31 +1,54 @@
-import { flags } from './CONST'
+// import { flags } from './CONST'
 
-export type Containers = [Int16Array, Int32Array, Int8Array, Uint16Array, Uint32Array, Uint8Array]
-export type Container = Containers[number]
+import JSIS from 'src'
 
-export type Configuration = {
-  range?: Range
+// export type Containers = [Int16Array, Int32Array, Int8Array, Uint16Array, Uint32Array, Uint8Array]
+// export type Container = Containers[number]
+
+// export type Configuration = {
+//   range?: Range
+//   size?: number
+// }
+
+// export type DataType = undefined | keyof typeof flags
+
+// export type Ranges = ['int8', 'int16', 'int32', 'uint8', 'uint16', 'uint32']
+// export type Range = Ranges[number]
+
+// export type EntryName = string
+// export type EntryType = number
+// export type EntryLength = undefined | number
+
+// export type EntryDefinition = [EntryName, EntryType, EntryLength]
+// export type Schema = EntryDefinition[]
+// export type Meta = Record<string, EntryDefinition>
+
+// export type Defaults = Required<
+//   Configuration & {
+//     bits: number
+//     header: number
+//     placeholder: number
+//     propCount: number
+//   }
+// >
+
+export type Encodable = string | boolean | number
+export type Decodable = number | number[]
+export type ROM = Int16Array
+
+export type FieldArguments = {
+  type?: (typeof JSIS.types)[number]
   size?: number
+  key?: string
 }
 
-export type DataType = undefined | keyof typeof flags
+export type Field = FieldArguments & {
+  name?: string
+  blocks: number
+  index: number
+}
 
-export type Ranges = ['int8', 'int16', 'int32', 'uint8', 'uint16', 'uint32']
-export type Range = Ranges[number]
-
-export type EntryName = string
-export type EntryType = number
-export type EntryLength = undefined | number
-
-export type EntryDefinition = [EntryName, EntryType, EntryLength]
-export type Schema = EntryDefinition[]
-export type Meta = Record<string, EntryDefinition>
-
-export type Defaults = Required<
-  Configuration & {
-    bits: number
-    header: number
-    placeholder: number
-    propCount: number
-  }
->
+export type Schema = {
+  range: number
+  fields: Record<string, Field>
+}
