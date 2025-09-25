5f3fa1a47d641927fe919dab56e41d0ee6fdf87a
Thomas van der Velde <contact@toolbarthomas.net>
2025-09-25 18:34:16 +0200

    chore(node): Include minified build

--- node/build.mjs
+++ node/build.mjs
@@ -1,20 +1,24 @@
-import esbuild from "esbuild";
+import esbuild from 'esbuild'
+import { parse } from '@toolbarthomas/argumentje'
 
 /**
  * Compiles the required modules.
  */
-(async () => {
+;(async () => {
+  const { minify } = parse()
   const defaults = {
     bundle: true,
-    entryPoints: ["./src/index.ts"],
+    entryPoints: ['./src/index.ts'],
     external: [],
+    minify,
     keepNames: true,
     metafile: false,
-    outdir: "dist",
-    platform: "browser",
-  };
+    outdir: 'dist',
+    platform: 'browser',
+    outExtension: { '.js': minify ? '.min.js' : '.js' }
+  }
 
-  esbuild.build({ ...defaults, format: "esm" }).then(() => {
-    console.info(`Package compiled.`);
-  });
-})();
+  esbuild.build({ ...defaults, format: 'esm' }).then(() => {
+    console.info(`Package compiled.`)
+  })
+})()
